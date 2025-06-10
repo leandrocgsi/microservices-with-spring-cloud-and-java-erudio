43e348288d0b7f4d558ba3f5268a2ddd6ec4d0b8
leandrocgsi <leandrocgsi@gmail.com>
2025-06-10 14:26:44 -0300

    Creating and Connecting Microservices with Spring Cloud and Feign - Exchange Microservice

--- 07_MicroservicesWithFeign/exchange-service/src/main/java/br/com/erudio/controller/ExchangeController.java
+++ 07_MicroservicesWithFeign/exchange-service/src/main/java/br/com/erudio/controller/ExchangeController.java
@@ -2,6 +2,7 @@ package br.com.erudio.controller;
 
 import br.com.erudio.environment.InstanceInformationService;
 import br.com.erudio.model.Exchange;
+import br.com.erudio.repository.ExchangeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +19,29 @@ public class ExchangeController {
     @Autowired
     InstanceInformationService informationService;
 
+    @Autowired
+    ExchangeRepository repository;
+
     // http://localhost:8000/exchange-service/5/USD/BRL
+    @GetMapping(value = "/{amount}/{from}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Exchange getExchange(
+            @PathVariable("amount") BigDecimal amount,
+            @PathVariable("from") String from,
+            @PathVariable("to") String to){
+
+        Exchange exchange = repository.findByFromAndTo(from, to);
+
+        if (exchange == null) throw new RuntimeException("Currency Unsupported!");
+
+        BigDecimal conversionFactor = exchange.getConversionFactor();
+        BigDecimal convertedValue = conversionFactor.multiply(amount);
+        exchange.setConvertedValue(convertedValue);
+        exchange.setEnvironment("PORT " + informationService.retrieveServerPort());
+
+        return exchange;
+    }
+
+    /*// http://localhost:8000/exchange-service/5/USD/BRL
     @GetMapping(value = "/{amount}/{from}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Exchange getExchange(
             @PathVariable("amount") BigDecimal amount,
@@ -26,5 +49,5 @@ public class ExchangeController {
             @PathVariable("to") String to){
         return new Exchange(1L, from, to, BigDecimal.ONE,
                 BigDecimal.ONE, "PORT " + informationService.retrieveServerPort());
-    }
+    }*/
 }
