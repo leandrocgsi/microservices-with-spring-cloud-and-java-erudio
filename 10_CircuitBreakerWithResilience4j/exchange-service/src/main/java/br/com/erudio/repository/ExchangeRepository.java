34b3347f10fd461d4c440f095dc5febaafdb2594
leandrocgsi <leandrocgsi@gmail.com>
2025-06-13 11:39:30 -0300

    Circuit Breaker with Resilience4j

---
+++ 10_CircuitBreakerWithResilience4j/exchange-service/src/main/java/br/com/erudio/repository/ExchangeRepository.java
@@ -0,0 +1,9 @@
+package br.com.erudio.repository;
+
+import br.com.erudio.model.Exchange;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeRepository extends JpaRepository<Exchange, Long> {
+
+    Exchange findByFromAndTo(String from, String to);
+}
