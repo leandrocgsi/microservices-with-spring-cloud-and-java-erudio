fc01ec6e74d14b37b070a7a0f65a7f62b520df78
leandrocgsi <leandrocgsi@gmail.com>
2025-06-13 11:40:32 -0300

    Working with Spring Doc OpenAPI (Swagger) - API Gateway

--- 11_SwaggerOpenAPIinMicroservices/api-gateway/src/main/java/br/com/erudio/config/ApiGatewayConfiguration.java
+++ 11_SwaggerOpenAPIinMicroservices/api-gateway/src/main/java/br/com/erudio/config/ApiGatewayConfiguration.java
@@ -1,3 +1,5 @@
+/*
+
 package br.com.erudio.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -11,14 +13,19 @@ public class ApiGatewayConfiguration {
     @Bean
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
         return builder.routes()
-                /*.route(p -> p.path("/get")
+                */
+/*.route(p -> p.path("/get")
                     .filters(f -> f
                         .addRequestHeader("Hello", "Wold")
                         .addRequestParameter("Hello", "Wold")
                     )
-                    .uri("http://httpbin.org:80"))*/
+                    .uri("http://httpbin.org:80"))*//*
+
                 .route(p -> p.path("/book-service/**").uri("lb://book-service"))
                 .route(p -> p.path("/exchange-service/**").uri("lb://exchange-service"))
                 .build();
     }
 }
+
+
+*/
--- 11_SwaggerOpenAPIinMicroservices/book-service/src/main/java/br/com/erudio/controller/BookController.java
+++ 11_SwaggerOpenAPIinMicroservices/book-service/src/main/java/br/com/erudio/controller/BookController.java
@@ -5,17 +5,16 @@ import br.com.erudio.environment.InstanceInformationService;
 import br.com.erudio.model.Book;
 import br.com.erudio.proxy.ExchangeProxy;
 import br.com.erudio.repository.BookRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Date;
-import java.util.HashMap;
-
+@Tag(name = "Book Endpoint")
 @RestController
 @RequestMapping("book-service")
 public class BookController {
@@ -30,6 +29,7 @@ public class BookController {
     private ExchangeProxy proxy;
 
     // http://localhost:8100/book-service/1/BRL
+    @Operation(summary = "Find a specific book by your ID")
     @GetMapping(value = "/{id}/{currency}",
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Book findBook(
